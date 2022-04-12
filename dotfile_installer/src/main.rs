8f985022dd73e68d2ef11b6be66f8e24a774a464
Pietro222222 <pietro.lima.queiroz@gmail.com>
2022-04-12 18:52:35 -0300

    a

--- dotfile_installer/src/main.rs
+++ dotfile_installer/src/main.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use serde_json::from_str;
 use pkg_manager::PkgWrapper;
 
 
@@ -10,12 +9,12 @@ struct Installer {
 }
 
 impl Installer {
-    pub fn install(&self, pkg:&PkgWrapper) {
+    pub fn install(&self, pkg:&mut PkgWrapper) {
         let pkg_list = self.pkgs.join(" ");
-        if let Err(e) = pkg.install_pkg(pkg_list) {
+        if let Err(_e) = pkg.install_pkg(pkg_list.clone()) {
             println!("Error while installing pkgs! {:#?}", pkg_list);
         }
-        std::process::Command::new("bash").arg(self.post_install).spawn().ok();
+        std::process::Command::new("bash").arg(&self.post_install).spawn().ok();
     } 
 }
 
@@ -34,10 +33,10 @@ fn main() {
     let string = std::fs::read_to_string("./packages.json").expect("packages.json doesnt exist!");
     let config: Config = serde_json::from_str(string.as_str()).expect("invalid packages.json file!");
     let mut pkg = PkgWrapper::new(true).unwrap();
-    config.bspwm.install(&pkg);
-    config.polybar.install(&pkg);
-    config.emacs.install(&pkg);
-    config.neovim.install(&pkg);
-    config.fish.install(&pkg);
-    config.alacritty.install(&pkg);
+    config.bspwm.install(&mut pkg);
+    config.polybar.install(&mut pkg);
+    config.emacs.install(&mut pkg);
+    config.neovim.install(&mut pkg);
+    config.fish.install(&mut pkg);
+    config.alacritty.install(&mut pkg);
 }
